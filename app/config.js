7c98f59f1ebbb2fe4023fafdb1998df8c6b0f413
Marcin_Borowski <marcin.boro1@interia.eu>
2020-05-29 22:28:54 +0200

    heroku connection fix try v.2

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const config = {
   databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://MarcinUser:qwer1234@cluster0-vd0a5.mongodb.net/test?retryWrites=true&w=majority',
-  port: 3000,
+  port: process.env.PORT || 3000,
   JwtSecret: process.env.JWT_SECRET || 'secret'
 
 }
